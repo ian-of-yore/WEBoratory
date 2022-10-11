7ee9f5f9e48c82df84f2e1e3c4f144c4319b9d67
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-12 01:40:19 +0600

    Did some customization on the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,7 @@
-import { Fragment } from 'react'
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import logo from '../../images/download.png'
-
-const navigation = [
-    { name: 'Home', href: '/' },
-    { name: 'Blog', href: '/blog' },
-    { name: 'Statistics', href: '/stats' }
-]
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
+import { Link } from 'react-router-dom'
 
 export default function Example() {
     return (
@@ -41,20 +31,10 @@ export default function Example() {
                                     <p className='text-white text-xl font-semibold'>WEBoratory</p>
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block py-4">
-                                    <div className="flex">
-                                        {navigation.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                className={classNames(
-                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                    'px-3 py-2 rounded-md text-xl font-semibold'
-                                                )}
-                                                aria-current={item.current ? 'page' : undefined}
-                                            >
-                                                {item.name}
-                                            </a>
-                                        ))}
+                                    <div className="flex text-white">
+                                        <Link to='/'>Home</Link>
+                                        <Link className='mx-4' to='/blog'>Blog</Link>
+                                        <Link to='/stats'>Statistics</Link>
                                     </div>
                                 </div>
                             </div>
@@ -63,20 +43,13 @@ export default function Example() {
 
                     <Disclosure.Panel className="sm:hidden">
                         <div className="space-y-1 px-2 pt-2 pb-3">
-                            {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                        'block px-3 py-2 rounded-md text-base font-medium'
-                                    )}
-                                    aria-current={item.current ? 'page' : undefined}
-                                >
-                                    {item.name}
-                                </Disclosure.Button>
-                            ))}
+                            <Disclosure.Button>
+                                <div className='flex flex-col text-white'>
+                                    <Link to='/'>Home</Link>
+                                    <Link className='my-2' to='/blog'>Blog</Link>
+                                    <Link to='/stats'>Statistics</Link>
+                                </div>
+                            </Disclosure.Button>
                         </div>
                     </Disclosure.Panel>
                 </>
